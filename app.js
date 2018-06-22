84a3967374b35d86b49bf7bdb565a1fa9c6973e1
Rehlaender <mario.rehlaender@accenture.com>
2018-06-22 16:16:08 -0500

    prepare server

--- app.js
+++ app.js
@@ -1,25 +1,82 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const port = 8080;
-app.use(express.static(path.join(__dirname, 'public')));
+const r = require('rethinkdb');
+const io = require('socket.io')();
 
-io.on('connection', (socket) => {
-  console.log('new connection made');
+function createDrawing({ connection, name }) {
+  return r.table('drawings')
+  .insert({
+    name,
+    timestamp: new Date(),
+  })
+  .run(connection)
+  .then(() => console.log('created a new drawing with name ', name));
+}
 
-  socket.emit('message-from-server', {
-    greeting: 'Hello from server'
+function subscribeToDrawings({ client, connection }) {
+  r.table('drawings')
+  .changes({ include_initial: true })
+  .run(connection)
+  .then((cursor) => {
+    cursor.each((err, drawingRow) => client.emit('drawing', drawingRow.new_val));
   });
+}
 
-  socket.on('message-from-client', (msg) => {
-    console.log(msg);
+function handleLinePublish({ connection, line, callback }) {
+  console.log('saving line to the db')
+  r.table('lines')
+  .insert(Object.assign(line, { timestamp: new Date() }))
+  .run(connection)
+  .then(callback);
+}
+
+function subscribeToDrawingLines({ client, connection, drawingId, from }) {
+  let query = r.row('drawingId').eq(drawingId);
+
+  if (from) {
+    query = query.and(r.row('timestamp').ge(new Date(from)))
+  }
+
+  return r.table('lines')
+  .filter(query)
+  .changes({ include_initial: true, include_types: true })
+  .run(connection)
+  .then((cursor) => {
+    cursor.each((err, lineRow) => client.emit(`drawingLine:${drawingId}`, lineRow.new_val));
+  });
+}
+
+r.connect({
+  host: 'localhost',
+  port: 28015,
+  db: 'awesome_whiteboard'
+}).then((connection) => {
+  io.on('connection', (client) => {
+    client.on('createDrawing', ({ name }) => {
+      createDrawing({ connection, name });
+    });
+
+    client.on('subscribeToDrawings', () => subscribeToDrawings({
+      client,
+      connection,
+    }));
+
+    client.on('publishLine', (line, callback) => handleLinePublish({
+      line,
+      connection,
+      callback,
+    }));
+
+    client.on('subscribeToDrawingLines', ({ drawingId, from }) => {
+      subscribeToDrawingLines({
+        client,
+        connection,
+        drawingId,
+        from,
+      });
+    });
   });
 });
 
-server.listen(port, () => {
-  console.log('listening on port' + port);
-})
 
-module.exports = app;
+const port = parseInt(process.argv[2], 10) || 5000;
+io.listen(port);
+console.log('listening on port ', port);
